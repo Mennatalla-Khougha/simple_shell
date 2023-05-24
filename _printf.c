8b9b1dba42b1f81134211ba143d420f02d846eb8
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-05-24 13:11:51 +0300

    a

--- _printf.c
+++ _printf.c
@@ -29,3 +29,51 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (len);
 }
+
+/**
+ * handle_dots - handle the dots
+ * @line: input
+ * @args: parameter of type para
+ */
+
+void handle_dots(char **line, para *args)
+{
+	int i = 0, len;
+	char *line_c = *line, *buffer;
+
+	if (line_c[0] == '.')
+	{
+		buffer = malloc(512);
+		if (buffer == NULL)
+		{
+			free(*line);
+			free_exit(args);
+		}
+		buffer[0] = '\0';
+		_strcat(buffer, &((*(args->pwd))[4]));
+		len = _strlen(buffer);
+		while (line_c[i])
+		{
+			if (line_c[i] == '.' && line_c[i + 1] == '.')
+			{
+				while (buffer[len - 1] != '/')
+					buffer[(len--) - 1] = '\0';
+				buffer[(len--) - 1] = '\0';
+				i++;
+			}
+			else if (line_c[i] != '/' && line_c[i] != '.')
+			{
+				buffer[len++] = '/';
+				while (line_c[i] && line_c[i] != '/')
+					buffer[len++] = line_c[i++];
+			}
+			i++;
+		}
+		buffer[len] = '\0';
+		if (access(buffer, X_OK) == 0)
+		{
+			free(*line);
+			*line = buffer;
+		}
+	}
+}
