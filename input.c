716584534bc6a86d1df675220183246b544359e6
George Raafat <georgexraafat@gmail.com>
2023-05-24 00:58:22 -0700

    a

---
+++ input.c
@@ -0,0 +1,185 @@
+#include "main.h"
+
+#define D(i) (*d)[i]
+/**
+* input - determine if we start new line in the shell with an arrow.
+* @args: parameter of type para
+* Return: 0, 1
+*/
+int input(para *args, int arrow)
+{
+	ssize_t read;
+
+	read = _getline(args);
+	if (read == -1)
+	{
+		if (isatty(STDIN_FILENO) && arrow && !args->file)
+			write(1, "\n", 1);
+		close(args->file);
+		free(args->line);
+		free(args->path);
+		free(*(args->pwd));
+		free(args->old_pwd);
+		exit(args->status);
+	}
+	if (args->line[read - 1] == '\n')
+	{
+		args->line[read - 1] = '\0';
+	}
+	if (args->line[read - 1] == ';')
+	{
+		args->line[read - 1] = '\0';
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * _getline - read an entire line from a stream.
+ * @args: parameter of type para.
+ * Return: no of chars read from the stream.
+*/
+ssize_t _getline(para *args)
+{
+	size_t i = 0;
+	ssize_t read_line;
+	char *buffer;
+	int buffer_size = 8;
+
+	if (&(args->line) == NULL)
+		return (-1);
+	buffer = _malloc(args, buffer_size + 1);
+	if (args->line)
+	{
+		free(args->line);
+		args->line = NULL;
+	}
+	while ((read_line = read(args->file, buffer + i, 1)) > 0)
+	{
+		i++;
+		buffer = handle_realloc(args, buffer, &buffer_size, 8, i);
+		if (buffer[i - 1] == '\n' || buffer[i - 1] == ';')
+			break;
+	}
+	if (read_line < 0 || (!read_line && !i))
+	{
+		free(buffer);
+		return (-1);
+	}
+	if (read_line != 8)
+		buffer[i] = '\0';
+	args->line = buffer;
+	return (i);
+}
+
+/**
+ * handle_input - determine if line contain $ or # and act accordingly.
+ * @args: parameter of type para..
+ *
+ * Return: 1 if the string is empty
+*/
+int handle_input(para *args)
+{
+char *buffer, *dollar, *start, *hash = _strchr(args->line, '#');
+int buffer_size = 1024;
+
+if (hash && ((hash != args->line && *(hash - 1) == ' ') || hash == args->line))
+	*hash = '\0';
+buffer = _malloc(args, buffer_size);
+dollar = _strchr(args->line, '$');
+start = args->line;
+buffer[0] = '\0';
+while (dollar)
+{
+	buffer = handle_realloc(args, buffer, &buffer_size,
+	1024, _strlen(buffer) + dollar - start);
+	_strncat(buffer, start, dollar - start);
+	handle_dollar(buffer, &dollar, args);
+	start = dollar;
+	dollar = _strchr(start, '$');
+}
+buffer = handle_realloc(args, buffer, &buffer_size,
+128, _strlen(buffer) + _strlen(start));
+_strcat(buffer, start);
+space(&buffer, args);
+free(args->line);
+args->line = buffer;
+return (buffer[0] == '\0');
+}
+
+/**
+ * handle_dollar - handle input containing $ sign.
+ * @buffer: input containing $.
+ * @d: input starting from $ sign.
+ * @args: parameter of type para.
+*/
+void handle_dollar(char *buffer, char **d, para *args)
+{
+	char num[10], *env;
+	int i = 0;
+
+	(*d)++;
+	if (D(0) == '$' || D(0) == '?')
+	{
+		if (D(0) == '$')
+			tostring(num, args->pid);
+		else
+			tostring(num, args->status);
+		_strcat(buffer, num);
+		(*d)++;
+	}
+	else
+	{
+		while (D(i) && D(i) != ' ' && D(i) != '#' && D(i) != '$' && D(i) != '/')
+			i++;
+		if (!i)
+			_strcat(buffer, "$");
+		else
+		{
+			env = _get_env(args->envp, *d, i);
+			if (env)
+				_strcat(buffer, env);
+			*d += i;
+		}
+	}
+}
+
+/**
+ * space - handle extra spaces in the input.
+ * @line: input
+ * @args: parameter of type para.
+*/
+void space(char **line, para *args)
+{
+	int i = 0, j = 0;
+	char *buffer = malloc(_strlen(*line) + 1);
+
+	if (buffer == NULL)
+	{
+		free(*line);
+		free_exit(args);
+	}
+	while ((*line)[i] && (*line)[i] == ' ')
+		i++;
+	while ((*line)[i])
+	{
+		if ((*line)[i] == ' ')
+		{
+			buffer[j] = ' ';
+			j++;
+			while ((*line)[i] == ' ')
+				i++;
+		}
+		else
+		{
+			buffer[j] = (*line)[i];
+			i++;
+			j++;
+		}
+	}
+	if (j && buffer[j - 1] == ' ')
+		j--;
+	buffer[j] = '\0';
+	free(*line);
+	*line = buffer;
+}
---
+++ main.h
@@ -0,0 +1,83 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#define STRING ((str) ? str : "(null)")
+
+/**
+ * struct para - structure
+ * @envp: array of enviromental variables.
+ * @pwd: the working directory
+ * @line: input.
+ * @path: the path
+ * @old_pwd: the old working directory
+ * @n_token: the number of token
+ * @n_path: the number of token in the path
+ * @status: the status of the last command
+ * @file: the type of stream
+ * @count: the count of operation
+ * @pid:the shell id
+ * @shell_name: the name of the shell
+ */
+typedef struct para
+{
+	char **envp;
+	char **pwd;
+	char *line;
+	char *shell_name;
+	char *path;
+	char *old_pwd;
+	int n_token;
+	int n_path;
+	int status;
+	int count;
+	int file;
+	int pid;
+} para;
+
+int _printf(const char *format, ...);
+int p_char(char c);
+int p_string(char *s);
+void p_number(int n);
+int num_len(int n);
+int specifier(char c, va_list args);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strtok(char *str, const char *delim);
+char *_strchr(char *s, char c);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *str1, char *str2);
+void tostring(char *str, int num);
+int _exceve(para *args, char *buff);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _atoi(char *s);
+int _exit_(para *args);
+int _command_(para *args);
+int input(para *args, int arrow);
+int token(char *line, char *delim);
+int rev_cmp(char *s1, char *s2);
+int _env(para *args);
+ssize_t _getline(para *args);
+void space(char **line, para *args);
+char *_get_env(char **envp, char *env, int len);
+int handle_input(para *args);
+void handle_dollar(char *buffer, char **dollar, para *args);
+char *handle_realloc(para *agrs, char *buffer, int *buffer_size, int extra,
+int max);
+char **get_PWD(para *args);
+int cd(para *args);
+void change_pwd(para *args);
+void free_exit(para *args);
+char *_malloc(para *args, int size);
+
+#endif
+
